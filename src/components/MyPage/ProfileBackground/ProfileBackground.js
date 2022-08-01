181440254e302f2607cae2cf90c6ac5bf40cf6a6
sh981013s <sh981013s@gmail.com>
2022-08-01 17:34:57 +0900

    style: 기본 회원탈퇴 버튼 생성

--- src/components/MyPage/ProfileBackground/ProfileBackground.js
+++ src/components/MyPage/ProfileBackground/ProfileBackground.js
@@ -1,7 +1,19 @@
+import styled from 'styled-components';
 import backgroundPic from 'images/profile_background.png';
 import { Container, UsernameText, CircleProfileImgContainer } from './styles';
 import { useRecoilValue } from 'recoil';
 import { userAtom } from 'states';
+import { Button } from '@mui/material';
+
+const WithDrawlBtn = styled(Button)`
+  && {
+    position: absolute;
+    right: 0;
+    bottom: -3rem;
+    background-color: #6315af;
+    color: pink;
+  }
+`;
 
 const ProfileBackground = () => {
   const user = useRecoilValue(userAtom);
@@ -11,6 +23,7 @@ const ProfileBackground = () => {
       <img src={backgroundPic} alt="" />
       <UsernameText>{user.username}</UsernameText>
       <CircleProfileImgContainer />
+      <WithDrawlBtn variant="contained">회원탈퇴</WithDrawlBtn>
     </Container>
   );
 };
