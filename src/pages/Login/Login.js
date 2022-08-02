66a9922563d70587460c2240a50ad62af2cb1125
김지형 <jihyoung9912@gmail.com>
2022-08-02 12:44:11 +0900

    feat: LoginUI 최종수정

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -9,9 +9,19 @@ import { useRecoilValue } from 'recoil';
 import { userAtom } from 'states';
 import styled from 'styled-components';
 
-const InputContainer = styled.div`
-  display: flex;
-  width: 10rem;
+const Logo = styled.div`
+  width: 7rem;
+  height: 7rem;
+  background: red;
+  border-radius: 50%;
+  margin-bottom: 1.5rem;
+`;
+
+const Input = styled.input`
+  margin-bottom: 0.7rem;
+  width: 13rem;
+  height: 2rem;
+  border-radius: 0.3rem;
 `;
 
 const Button = styled.button`
@@ -26,6 +36,7 @@ const Button = styled.button`
   outline: 0;
   cursor: pointer;
   margin-top: 0.6rem;
+  margin-bottom: 0.2rem;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.1);
   transition: all 0.3s ease-out;
   :hover {
@@ -54,13 +65,14 @@ const Login = () => {
 
   return (
     <WrapContainer>
+      <Logo />
       <form onSubmit={handleSubmit(submit)}>
-        <InputContainer>
-          <input placeholder="ID" type="text" {...register('username')} />
-          <div>{errors.username?.message}</div>
-        </InputContainer>
         <div>
-          <input
+          <Input placeholder="ID" type="text" {...register('username')} />
+          <div>{errors.username?.message}</div>
+        </div>
+        <div>
+          <Input
             placeholder="Password"
             type="password"
             {...register('password')}
