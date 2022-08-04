c1228e5b142c712720b14de9628d001dcca5694d
sh981013s <sh981013s@gmail.com>
2022-08-04 14:07:56 +0900

    feat: myproducts url 생성

--- src/apis/productsRelatredAPI.js
+++ src/apis/productsRelatredAPI.js
@@ -3,15 +3,20 @@ import axiosInstance from './config';
 const client = axiosInstance;
 
 const productsRelatedAPI = {
-  getProductLists: () => {
+  getProductCategoryLists: () => {
     return client.get('products/category/');
   },
+
   postProduct: (payload) => {
-    return client.post('/products/', payload, {
+    return client.post('products/', payload, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
     });
+  },
+
+  getMyProducts: () => {
+    return client.get('products/');
   },
 };
 
