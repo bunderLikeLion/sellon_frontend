d1b67de9a0859be7bcac70919d74d49d96947d53
김지형 <jihyoung9912@gmail.com>
2022-08-05 20:55:14 +0900

    feat: AuctionDeatail 이미지 container 수정

--- src/components/Auction/AuctionDetail/AuctionItem.js
+++ src/components/Auction/AuctionDetail/AuctionItem.js
@@ -1,7 +1,6 @@
 import CardMedia from '@mui/material/CardMedia';
 import Card from '@mui/material/Card';
 import styled from 'styled-components';
-import CardHeader from '@mui/material/CardHeader';
 
 const Container = styled(Card)`
   display: flex;
@@ -10,50 +9,40 @@ const Container = styled(Card)`
   height: 100%;
   align-items: center;
   justify-content: center;
+  background: transparent !important;
 `;
 
-const CardTop = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: left;
-  align-items: center;
-  margin-left: 2rem;
-`;
-
-const CardBottom = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: left;
-  align-items: center;
+const ItemImgContainer = styled.div`
   position: relative;
-`;
-
-const PersonCntBox = styled.span`
-  display: inline-flex;
-  padding: 0.4rem 0.8rem;
-  margin-bottom: 0.5rem;
-  height: 2.4rem;
-  background: #101010;
-  border: 1px solid grey;
-  border-radius: 1rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-`;
-
-const EnterBox = styled(PersonCntBox)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-left: 2rem;
-  font-size: 1rem;
-  width: 15rem;
+  width: 100%;
+  height: 20rem;
 `;
 
 const ItemImg = styled(CardMedia)`
-  margin-left: auto;
-  margin-right: auto;
-  width: 60% !important;
-  height: 75% !important;
+  width: 90% !important;
+  height: 100% !important;
+  border-radius: 0.5rem;
+  object-fit: cover !important;
+  object-position: center;
+`;
+
+const ItemDurationContainer = styled.div`
+  position: absolute;
+  top: 3%;
+  right: 15%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 12%;
+  height: 9%;
+  border-radius: 1rem;
+  background: purple;
+`;
+
+const ItemDuration = styled.h1`
+  font-size: 1rem;
+  font-weight: 400;
+  color: white;
 `;
 
 const ItemContentContainer = styled.div`
@@ -61,20 +50,66 @@ const ItemContentContainer = styled.div`
   height: 100%;
 `;
 
+const ItemNameContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 13%;
+`;
+
+const ItemName = styled.h1`
+  color: #fff;
+  font-size: 2rem;
+  font-weight: 350;
+`;
+
+const ItemDescriptionContainer = styled.div`
+  width: 80%;
+  overflow-y: scroll;
+  max-height: 20rem;
+`;
+
+const ItemDescription = styled.p`
+  color: #fff;
+  line-height: 1.6rem;
+  font-size: 1.3rem;
+  font-weight: 200;
+`;
+
 const AuctionItem = () => {
   return (
     <Container sx={{ maxWidth: '100%' }}>
-      <ItemImg
-        component="img"
-        image="https://cdn.shopify.com/s/files/1/0087/6193/3920/products/1904760_SMOK_1_300x300@2x.jpg?v=1656086629"
-      />
+      <ItemImgContainer>
+        <ItemImg
+          component="img"
+          image="https://cdn.shopify.com/s/files/1/0087/6193/3920/products/1904760_SMOK_1_300x300@2x.jpg?v=1656086629"
+        />
+        <ItemDurationContainer>
+          <ItemDuration>D - 7</ItemDuration>
+        </ItemDurationContainer>
+      </ItemImgContainer>
       <ItemContentContainer>
-        <CardTop>
-          <CardHeader title="Stussy 8  ball Tee" />
-        </CardTop>
-        <CardBottom>
-          <EnterBox>2022.8.3 ~ 2022.8.10</EnterBox>
-        </CardBottom>
+        <ItemNameContainer>
+          <ItemName>NIKE 신발 사세요!</ItemName>
+        </ItemNameContainer>
+        <ItemDescriptionContainer>
+          <ItemDescription>
+            이 신발은 영국에서 시작되어 되게 되게 잘 팔리는 핫한 잇템입니다.
+            코드 적는 것보다 이런 데에 무슨 멘트 적는지 고민하는게 더
+            오래걸리네요.Contrary to popular belief, Lorem Ipsum is not simply
+            random text. It has roots in a piece of classical Latin literature
+            from 45 BC, making it over 2000 years old. Richard McClintock, a
+            Latin professor at Hampden-Sydney College in Virginia, looked up one
+            of the more obscure Latin words, consectetur, from a Lorem Ipsum
+            passage, and going through the cites of the word in classical
+            literature, discovered the undoubtable source. Lorem Ipsum comes
+            from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
+            (The Extremes of Good and Evil) by Cicero, written in 45 BC. This
+            book is a treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+            amet..", comes from a line in section 1.10.32.
+          </ItemDescription>
+        </ItemDescriptionContainer>
       </ItemContentContainer>
     </Container>
   );
--- src/components/Auction/AuctionDetail/ValidationModal.js
+++ src/components/Auction/AuctionDetail/ValidationModal.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 
 const ModalContainer = styled(Box)`
   position: absolute;
