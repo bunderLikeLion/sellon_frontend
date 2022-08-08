a3131edbb54234ac6c9fde8ccfb546449d6a174f
Chaewon14 <rabbit7653@naver.com>
2022-08-09 00:59:36 +0900

    REFACTOR: AddItemModal.js UI코드 리팩토링

--- src/components/MyPage/ItemAddForm/ImageDragDrop/styles.js
+++ src/components/MyPage/ItemAddForm/ImageDragDrop/styles.js
@@ -5,9 +5,19 @@ export const ThumbContainer = styled.aside``;
 
 export const MultiThumbContainer = styled.aside`
   display: flex;
-  width: 100%;
+  width: 13rem;
   flex-direction: row;
   flex-wrap: wrap;
+  border: 3px solid transparent;
+  background-image: linear-gradient(
+      ${(props) => props.theme.color_background__default},
+      ${(props) => props.theme.color_background__default}
+    ),
+    ${(props) => props.theme.color_border__hover} !important;
+  color: #ffffff !important;
+  background-origin: border-box !important;
+  background-clip: content-box, border-box !important;
+  border-radius: 1rem;
 `;
 
 export const Thumb = styled.div`
@@ -35,19 +45,31 @@ export const Container = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 1rem;
-  border: 2px solid transparent;
-  border-image: ${(props) => props.theme.color_border__hover} 1;
+  border: 3px solid transparent;
+  background-image: linear-gradient(
+      ${(props) => props.theme.color_background__default},
+      ${(props) => props.theme.color_background__default}
+    ),
+    ${(props) => props.theme.color_border__hover} !important;
+  color: #ffffff !important;
+  background-origin: border-box !important;
+  background-clip: content-box, border-box !important;
+  border-radius: 1rem;
   width: 6rem;
-  font-size: 0.6rem;
 
   .dropzone {
     margin: 10px;
-    padding: 1rem;
     width: 100%;
     height: 4rem;
-    border: 2px solid transparent;
-    border-image: ${(props) => props.theme.color_border__hover} 1;
+    border: 3px solid transparent;
+    background-image: linear-gradient(
+        ${(props) => props.theme.color_background__default},
+        ${(props) => props.theme.color_background__default}
+      ),
+      ${(props) => props.theme.color_border__hover} !important;
+    background-origin: border-box !important;
+    background-clip: content-box, border-box !important;
+    border-radius: 1rem;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -58,18 +80,21 @@ export const Container = styled.section`
 export const MultipleContainer = styled.section`
   display: flex;
   align-items: center;
-  margin-top: 1rem;
-  border: 2px solid transparent;
-  border-image: ${(props) => props.theme.color_border__hover} 1;
-  width: 13rem;
   font-size: 0.6rem;
 
   .dropzone {
     margin: 10px;
     width: 5rem;
     height: 5rem;
-    border: 2px solid transparent;
-    border-image: ${(props) => props.theme.color_border__hover} 1;
+    border: 3px solid transparent;
+    background-image: linear-gradient(
+        ${(props) => props.theme.color_background__default},
+        ${(props) => props.theme.color_background__default}
+      ),
+      ${(props) => props.theme.color_border__hover} !important;
+    background-origin: border-box !important;
+    background-clip: content-box, border-box !important;
+    border-radius: 1rem;
     display: flex;
     justify-content: center;
     align-items: center;
