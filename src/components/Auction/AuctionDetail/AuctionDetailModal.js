568b15f6b082ed53e26671b68015701f429680bd
김지형 <jihyoung9912@gmail.com>
2022-08-13 19:04:27 +0900

    feat: Auction Detail Modal 수정

--- src/components/Auction/AuctionDetail/AuctionDetailModal.js
+++ src/components/Auction/AuctionDetail/AuctionDetailModal.js
@@ -81,9 +81,9 @@ const AuctionDetailModal = ({
                 isTriggeredFromModal={true}
               />
             )}
-            {singleItemDataFetched && (
-              <UserInformation singleItemData={singleItemData} />
-            )}
+            {/*{singleItemDataFetched && (*/}
+            {/*  <UserInformation singleItemData={singleItemData} />*/}
+            {/*)}*/}
           </UserUploadContainer>
           <ItemDetailContainer>
             {singleItemDataFetched && (
--- src/components/MyPage/ItemDetail/ItemImage.js
+++ src/components/MyPage/ItemDetail/ItemImage.js
@@ -68,6 +68,8 @@ const ItemImage = ({ singleItemData, isTriggeredFromModal }) => {
             if (!singleImg) return null;
             return <SubPic key={singleImg?.id} image={singleImg?.file} />;
           })}
+        {thumbnail && <MainPic image={thumbnail?.file} />}
+        {thumbnail && <MainPic image={thumbnail?.file} />}
       </SubPicContainer>
     </>
   );
