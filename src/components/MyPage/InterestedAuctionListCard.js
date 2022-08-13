fc97ba707cb10e6f8df165d3f28c591ac2263ede
yoojinyang0303 <[yoojinyang0303@gmail.com]>
2022-08-14 02:57:07 +0900

    feat: 마이페이지-내가 올린 경매 UI 작성

--- src/components/MyPage/InterestedAuctionListCard.js
+++ src/components/MyPage/InterestedAuctionListCard.js
@@ -17,7 +17,6 @@ const StyledWrapContainer = styled.div`
 const CardContainer = styled(Card)`
   position: relative;
   width: 30%;
-  margin-right: 2rem;
   margin-bottom: 4.5rem;
   border-radius: 3rem !important;
   color: ${(props) => props.theme.color_font__secondary} !important;
@@ -28,6 +27,11 @@ const CardContainer = styled(Card)`
     transition: 0.5s;
     transform: translateY(-0.5rem);
   };
+
+  :nth-child(2),
+  :nth-child(5) {
+    margin: 0 3rem 4.5rem 3rem;
+  }
   }
 `;
 
