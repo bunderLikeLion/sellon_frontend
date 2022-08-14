85f687d8b4260a02e9557d792b4e5105bc6a9683
김지형 <jihyoung9912@gmail.com>
2022-08-15 03:19:58 +0900

    feat: InterestedAUctionListCard

--- src/components/MyPage/InterestedAuctionListCard.js
+++ src/components/MyPage/InterestedAuctionListCard.js
@@ -20,17 +20,15 @@ const CardContainer = styled(Card)`
   border-radius: 3rem !important;
   color: ${(props) => props.theme.color_font__secondary} !important;
   background: ${(props) => props.theme.color_background__primary} !important;
-  box-shadow: 0 0 4px 7px ${(props) =>
-    props.theme.color_border__topleft} !important;
-  :hover{
+  box-shadow: 0 0 4px 7px ${(props) => props.theme.color_border__topleft} !important;
+  :hover {
     transition: 0.5s;
     transform: translateY(-0.5rem);
-  };
+  }
 
   :nth-child(2),
   :nth-child(5) {
     margin: 0 3rem 4.5rem 3rem;
-  }
   }
 `;
 
@@ -38,34 +36,35 @@ const CardTop = styled.div`
   width: 100%;
   display: flex;
   justify-content: start;
-  padding: 1rem 1rem 1rem 1.5rem;
   align-items: center;
+  padding: 1rem 1rem 1rem 1.5rem;
 `;
 
 const CardProfile = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 2rem;
   height: 2rem;
   padding: 1.5rem;
   border-radius: 50%;
   background: #c9c9c9;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 `;
 
 const CardBottom = styled.div`
   width: 100%;
   padding: 0 1rem 1rem 1rem;
   display: flex;
-  position: relative;
   align-items: center;
   justify-content: space-between;
+  position: relative;
 `;
 
 const PersonCntBox = styled.span`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+
   margin: 0.3rem;
   width: 5rem;
   height: 2.4rem;
@@ -108,6 +107,15 @@ const StyledCancelOutlinedIcon = styled(CancelOutlinedIcon)`
 `;
 
 export const FinishedOverlay = styled(Card)`
+  display: ${(props) => (props.isFinished ? 'flex' : 'none')};
+  position: absolute;
+  left: 0;
+  top: 0;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  font-size: 2rem;
   background-color: rgba(57, 57, 65, 0.83) !important;
 `;
 
@@ -115,106 +123,6 @@ const InterestedAuctionListCard = () => {
   return (
     <StyledWrapContainer>
       {/*Card_01*/}
-      <CardContainer sx={{ maxWidth: '100%' }}>
-        <StyledCancelOutlinedIcon />
-        <MyCardMedia
-          component="img"
-          height="150"
-          image="https://post-phinf.pstatic.net/MjAxOTA2MjhfMTk3/MDAxNTYxNjg3MTY2OTQ2.OXRI7eorUbDI_4lIP1YlGHL_6ZMhh6Zgn4U7POAMCHMg.ygJy1cG5GZZxMvJ-0xqEKLdVEBZj13acwYC-Cri56BMg.JPEG/candyofthemonthclub.jpg?type=w1200"
-        />
-        <CardTop>
-          <CardProfile />
-          <MyCardHeader>사탕 살래?</MyCardHeader>
-        </CardTop>
-        <CardBottom style={{ width: '100%' }}>
-          <PersonCntBox>
-            <PersonIcon />
-            30명
-          </PersonCntBox>
-          <EnterBox>D-7</EnterBox>
-        </CardBottom>
-      </CardContainer>
-      {/*Card_02*/}
-      <CardContainer sx={{ maxWidth: '100%' }}>
-        <StyledCancelOutlinedIcon />
-        <MyCardMedia
-          component="img"
-          height="150"
-          image="https://hhsmedia.com/wp-content/uploads/2018/10/candies-e1541131061529-900x600.jpg"
-        />
-        <CardTop>
-          <CardProfile />
-          <MyCardHeader>사탕 살래?</MyCardHeader>
-        </CardTop>
-        <CardBottom style={{ width: '100%' }}>
-          <PersonCntBox>
-            <PersonIcon />
-            30명
-          </PersonCntBox>
-          <EnterBox>D-7</EnterBox>
-        </CardBottom>
-      </CardContainer>
-      {/*Card_03*/}
-      <CardContainer sx={{ maxWidth: '100%' }}>
-        <StyledCancelOutlinedIcon />
-        <MyCardMedia
-          component="img"
-          height="150"
-          image="https://post-phinf.pstatic.net/MjAxOTA2MjhfMTk3/MDAxNTYxNjg3MTY2OTQ2.OXRI7eorUbDI_4lIP1YlGHL_6ZMhh6Zgn4U7POAMCHMg.ygJy1cG5GZZxMvJ-0xqEKLdVEBZj13acwYC-Cri56BMg.JPEG/candyofthemonthclub.jpg?type=w1200"
-        />
-        <CardTop>
-          <CardProfile />
-          <MyCardHeader>사탕 살래?</MyCardHeader>
-        </CardTop>
-        <CardBottom style={{ width: '100%' }}>
-          <PersonCntBox>
-            <PersonIcon />
-            30명
-          </PersonCntBox>
-          <EnterBox>D-7</EnterBox>
-        </CardBottom>
-      </CardContainer>
-      {/*Card_04*/}
-      <CardContainer sx={{ maxWidth: '100%' }}>
-        <StyledCancelOutlinedIcon />
-        <MyCardMedia
-          component="img"
-          height="150"
-          image="https://post-phinf.pstatic.net/MjAxOTA2MjhfMTk3/MDAxNTYxNjg3MTY2OTQ2.OXRI7eorUbDI_4lIP1YlGHL_6ZMhh6Zgn4U7POAMCHMg.ygJy1cG5GZZxMvJ-0xqEKLdVEBZj13acwYC-Cri56BMg.JPEG/candyofthemonthclub.jpg?type=w1200"
-        />
-        <CardTop>
-          <CardProfile />
-          <MyCardHeader>사탕 살래?</MyCardHeader>
-        </CardTop>
-        <CardBottom style={{ width: '100%' }}>
-          <PersonCntBox>
-            <PersonIcon />
-            30명
-          </PersonCntBox>
-          <EnterBox>D-7</EnterBox>
-        </CardBottom>
-      </CardContainer>
-      {/*Card_05*/}
-      <CardContainer sx={{ maxWidth: '100%' }}>
-        <StyledCancelOutlinedIcon />
-        <MyCardMedia
-          component="img"
-          height="150"
-          image="https://hhsmedia.com/wp-content/uploads/2018/10/candies-e1541131061529-900x600.jpg"
-        />
-        <CardTop>
-          <CardProfile />
-          <MyCardHeader>사탕 살래?</MyCardHeader>
-        </CardTop>
-        <CardBottom style={{ width: '100%' }}>
-          <PersonCntBox>
-            <PersonIcon />
-            30명
-          </PersonCntBox>
-          <EnterBox>D-7</EnterBox>
-        </CardBottom>
-      </CardContainer>
-      {/*Card_06*/}
       <CardContainer sx={{ maxWidth: '100%' }}>
         <StyledCancelOutlinedIcon />
         <MyCardMedia
