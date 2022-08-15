dc20137743bd8ffa106d2287462107f2faadfd8c
shinkeonkim <singun11@kookmin.ac.kr>
2022-08-15 21:37:55 +0900

    fix: 중첩 a 태그 오류 수정

--- src/pages/Navbar/Navbar.js
+++ src/pages/Navbar/Navbar.js
@@ -64,7 +64,6 @@ const ResponsiveAppBar = () => {
             <Typography
               variant="h6"
               noWrap
-              component="a"
               sx={{
                 mr: 2,
                 display: { xs: 'none', md: 'flex' },
@@ -86,7 +85,6 @@ const ResponsiveAppBar = () => {
           <Typography
             variant="h5"
             noWrap
-            component="a"
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
