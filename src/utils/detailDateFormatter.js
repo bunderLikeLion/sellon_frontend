07e411322b49f986c2de4a9da19d7693c93cf7bc
sh981013s <sh981013s@gmail.com>
2022-08-16 21:45:11 +0900

    refactor: 진행중인 거래 시간 출력 logic refactored

--- src/utils/detailDateFormatter.js
+++ src/utils/detailDateFormatter.js
@@ -1,12 +1,22 @@
 const detailDateFormatter = (givenDate) => {
   const date = new Date(givenDate);
+  const today = new Date();
   const months = date.getMonth() < 10 ? '0' + date.getMonth() : date.getMonth();
   const days = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
-  const hrs = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+  let hrs = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+  const ampm = hrs >= 12 ? 'PM' : 'AM';
+  hrs = hrs % 12;
+  hrs = hrs ? hrs : 12;
   const mins =
     date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
 
-  return months + '.' + days + ' ' + hrs + ':' + mins;
+  if (
+    date.getMonth() === today.getMonth() &&
+    date.getDate() === today.getDate()
+  )
+    return hrs + ':' + mins + ' ' + ampm;
+
+  return months + '.' + days + ' ' + hrs + ':' + mins + ' ' + ampm;
 };
 
 export default detailDateFormatter;
