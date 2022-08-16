16e9715400ef3a05c2e997a39f09ff052ca3919a
shinkeonkim <singun11@kookmin.ac.kr>
2022-08-17 01:49:46 +0900

    chore: 관심 경매로 등록된 경매가 없는 경우 placeholder 추가

--- src/components/MyPage/InterestedAuctionList.js
+++ src/components/MyPage/InterestedAuctionList.js
@@ -6,6 +6,7 @@ import { Pagination } from '@mui/material';
 import { useState } from 'react';
 import { useMyProductsQuery } from 'queries/product';
 import AuctionListContainer from 'components/Shared/AuctionListContainer';
+import EmptyListPlaceHolder from 'components/Shared/EmptyListPlaceholder';
 
 const FlexContainer = styled(AuctionListContainer)`
   margin-top: 2rem;
@@ -54,16 +55,20 @@ const InterestedAuctionList = () => {
       {interestedAuctionListsFetched && (
         <>
           <p>총 {interestedAuctionLists?.total_count}개</p>
-          <FlexContainer>
-            {interestedAuctionLists?.results.map((singleInterestedAuction) => (
-              <InterestedAuctionListCard
-                data={singleInterestedAuction.auction}
-                isFinished={isAuctionFinishedHandler(
-                  singleInterestedAuction?.auction?.end_at
-                )}
-              />
-            ))}
-          </FlexContainer>
+          {
+            interestedAuctionLists?.total_count > 0 ? (
+              <FlexContainer>
+                {interestedAuctionLists?.results.map((singleInterestedAuction) => (
+                  <InterestedAuctionListCard
+                    data={singleInterestedAuction.auction}
+                    isFinished={isAuctionFinishedHandler(
+                      singleInterestedAuction?.auction?.end_at
+                    )}
+                  />
+                ))}
+              </FlexContainer>
+            ) : <EmptyListPlaceHolder message="아직 관심 경매로 등록한 경매가 없습니다. 😅" />
+          }
         </>
       )}
 
