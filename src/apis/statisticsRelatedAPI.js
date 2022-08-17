3dc41ab6ed694fd72cfb427141f5b79294921e5a
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-08-18 03:00:16 +0900

    feat: 실시간 거래 랭킹 API 연결

--- src/apis/statisticsRelatedAPI.js
+++ src/apis/statisticsRelatedAPI.js
@@ -12,6 +12,10 @@ const statisticsRelatedAPI = {
   getMonthlyChampion: () => {
     return client.get('statistics/monthly_champion/').then((res) => res.data);
   },
+
+  getDealingRanking: () => {
+    return client.get('statistics/dealing_ranking/').then((res) => res.data);
+  },
 };
 
 export default statisticsRelatedAPI;
