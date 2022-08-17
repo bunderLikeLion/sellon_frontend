e152944125c4e3ee1283e5e3885b1edd49c4092d
Chaewon14 <rabbit7653@naver.com>
2022-08-17 22:58:26 +0900

    refactor: 경매발행 페이지, 인벤토리 모달창 UI 리팩토링

--- src/components/Auction/AuctionPublish/InventoryItem.js
+++ src/components/Auction/AuctionPublish/InventoryItem.js
@@ -15,37 +15,67 @@ const Container = styled(Card)`
   margin: 2%;
   border-radius: 1rem !important;
   cursor: pointer;
-  border: 3.5px solid ${(props) => props.theme.color_border__topleft};
-  border-right: 3.5px solid ${(props) => props.theme.color_border__bottomright};
-  border-bottom: 3.5px solid ${(props) => props.theme.color_border__bottomright};
+  border: 0.1rem solid transparent;
+  // border: 3.5px solid ${(props) => props.theme.color_border__topleft};
+  // border-right: 3.5px solid ${(props) =>
+    props.theme.color_border__bottomright};
+  // border-bottom: 3.5px solid ${(props) =>
+    props.theme.color_border__bottomright};
   background: ${(props) => props.theme.color_background__primary} !important;
   :hover {
-    transition: 0.2s;
-    box-shadow: 0 5px 15px 5px ${(props) => props.theme.color_border__topleft};
-    transform: translateY(-5px);
+    border: 0.1rem solid ${(props) => props.theme.color_border__hover__light} !important;
   }
 `;
 
 const ImgContainer = styled.div`
   width: 100%;
-  height: 70%;
+  height: 9rem;
 `;
 
 const ItemContentContainer = styled.div`
   display: flex;
   align-items: center;
   width: 100%;
-  height: 30%;
+  height: 6rem;
+  padding: 1rem;
   background: ${(props) => props.theme.color_background__primary};
   font-size: 100rem !important;
 `;
 
 const CardTop = styled.div`
+  width: 100%;
   color: ${(props) => props.theme.color_font__secondary};
 `;
 
 const UnableCard = styled(EnabledOverlay)`
+  padding: 1rem;
+  font-size: 1.3rem;
+  line-height: 1.8rem;
   box-shadow: 0 0 4px 7px ${(props) => props.theme.color_background__default} !important;
+`;
+
+const CardHeaderTitle = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis !important;
+  white-space: nowrap;
+  font-size: 1.2rem;
+  padding: 1rem 0;
+`;
+
+const CategoryBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: fit-content;
+  height: fit-content;
+  padding: 0.3rem 1rem;
+  font-size: 0.8rem;
+  margin-bottom: 1rem;
+  border-radius: 20px;
+  color: ${(props) => props.theme.color_buttontext__ok};
+  background: ${(props) => props.theme.color_background__success};
 `;
 
 const InventoryItem = ({
@@ -68,7 +98,9 @@ const InventoryItem = ({
       </ImgContainer>
       <ItemContentContainer>
         <CardTop>
-          <CardHeader title={singleItem?.name} />
+          {/* <CardHeader title={singleItem?.name} /> */}
+          <CardHeaderTitle>{singleItem?.name}</CardHeaderTitle>
+          <CategoryBox>{singleItem?.product_category.name}</CategoryBox>
         </CardTop>
         {status && (
           <UnableCard>
