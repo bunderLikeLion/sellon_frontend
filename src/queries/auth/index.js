b928ffd989496cfa2975d1b4ce0c48242fff0e1e
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-08-17 21:35:06 +0900

    refactor: auth 관련 api와 단순 정보 api 분리

--- src/queries/auth/index.js
+++ src/queries/auth/index.js
@@ -1,4 +1,3 @@
 export { default as useLoginMutation } from './useLoginMutation';
 export { default as useSignInMutation } from './useSignInMutation';
 export { default as useWithdrawlMutation } from './useWithdrawlMutation';
-export { default as useRatingQuery } from './useRatingQuery';
