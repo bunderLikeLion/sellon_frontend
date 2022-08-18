9272e5dc70548b502f34f4b5c69d52750b131c67
shinkeonkim <singun11@kookmin.ac.kr>
2022-08-18 19:33:52 +0900

    feat: 회원가입 페이지 반응형 디자인

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -18,24 +18,41 @@ const Container = styled.div`
 
 const Card = styled.div`
   display: flex;
+  max-width: 90%;
   width: 60rem;
   height: 34rem;
   box-shadow: 0 5px 20px rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+
+  @media screen and (max-width: 1000px) {
+    flex-direction: column-reverse;
+  }
 `;
 
 const Form = styled.div`
+  flex: 1;
   display: flex;
-  flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-  width: 50%;
-  height: 80%;
-  margin-top: 7%;
+  flex-wrap: wrap;
+  padding: 2rem;
+  align-content: center;
+  gap: 4rem;
+
+  @media screen and (max-width: 1000px) {
+    flex-direction: column;
+    flex-grow: 1;
+    flex-shrink: 1;
+    flex-basis: auto;
+    width: 100%;
+    gap: 2rem;
+  }
 `;
 
 const GuideContainer = styled.div`
-  width: 65%;
-  margin-bottom: 2.5rem;
+  display: flex;
+  justify-content: flex-start;
+  text-align: left;
 `;
 
 const Guide = styled.h1`
@@ -45,9 +62,9 @@ const Guide = styled.h1`
 
 const InputContainer = styled.div`
   display: flex;
-  flex-wrap: wrap;
   justify-content: center;
-  width: 70%;
+  flex-wrap: wrap;
+  width: 100%;
   height: 4rem;
 `;
 
@@ -95,9 +112,27 @@ const Button = styled.button`
 `;
 
 const Img = styled.img`
-  width: 50%;
+  flex: 1;
   height: 100%;
+  border-radius: 0 10px 10px 0;
+
+  @media screen and (max-width: 1000px) {
+    border-radius: 10px 10px 0 0;
+
+    flex-grow: 1;
+    flex-shrink: 1;
+    flex-basis: auto;
+    width: auto;
+    max-height: 15rem;
+  }
 `;
+
+
+const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
 
 const Register = () => {
   const navigate = useNavigate();
@@ -136,50 +171,51 @@ const Register = () => {
           <GuideContainer>
             <Guide>회원가입</Guide>
           </GuideContainer>
+          <InputsContainer>
+            <InputContainer>
+              <Input
+                placeholder="아이디"
+                type="text"
+                value={username}
+                onChange={handleUsername}
+                onKeyPress={onKeyPressFunc}
+              />
+            </InputContainer>
 
-          <InputContainer>
-            <Input
-              placeholder="아이디"
-              type="text"
-              value={username}
-              onChange={handleUsername}
-              onKeyPress={onKeyPressFunc}
-            />
-          </InputContainer>
+            <InputContainer>
+              <Input
+                placeholder="이메일"
+                type="email"
+                value={email}
+                onChange={handleEmail}
+                onKeyPress={onKeyPressFunc}
+              />
+            </InputContainer>
 
-          <InputContainer>
-            <Input
-              placeholder="이메일"
-              type="email"
-              value={email}
-              onChange={handleEmail}
-              onKeyPress={onKeyPressFunc}
-            />
-          </InputContainer>
+            <InputContainer>
+              <Input
+                placeholder="비밀번호"
+                type="password"
+                value={password1}
+                onChange={handlePassword1}
+                onKeyPress={onKeyPressFunc}
+              />
+            </InputContainer>
 
-          <InputContainer>
-            <Input
-              placeholder="비밀번호"
-              type="password"
-              value={password1}
-              onChange={handlePassword1}
-              onKeyPress={onKeyPressFunc}
-            />
-          </InputContainer>
+            <InputContainer>
+              <Input
+                placeholder="비밀번호 확인"
+                type="password"
+                value={password2}
+                onChange={handlePassword2}
+                onKeyPress={onKeyPressFunc}
+              />
+            </InputContainer>
 
-          <InputContainer>
-            <Input
-              placeholder="비밀번호 확인"
-              type="password"
-              value={password2}
-              onChange={handlePassword2}
-              onKeyPress={onKeyPressFunc}
-            />
-          </InputContainer>
-
-          <ButtonContainer>
-            <Button onClick={submit}>가입</Button>
-          </ButtonContainer>
+            <ButtonContainer>
+              <Button onClick={submit}>가입</Button>
+            </ButtonContainer>
+          </InputsContainer>
         </Form>
         <Img src={SignPic} />
       </Card>
