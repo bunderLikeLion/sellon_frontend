c8f2a98d11b764140ca5ec6657a54b901e716cbd
sh981013s <sh981013s@gmail.com>
2022-08-27 01:10:03 +0900

    refactor: statushandler non-reachable code 수정

--- src/utils/statusHandler.js
+++ src/utils/statusHandler.js
@@ -2,21 +2,16 @@ const statusHandler = (status) => {
   switch (status) {
     case 1:
       return '최하';
-      break;
     case 2:
       return '중하';
-      break;
     case 3:
       return '중';
-      break;
     case 4:
       return '중상';
-      break;
     case 5:
       return '최상';
-      break;
     default:
-      break;
+      return '최상';
   }
 };
 
